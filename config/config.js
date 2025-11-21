87b10267c0a9243610d839a612e1df3b74eac6db
Olalekan Macaulay <olalekan.macaulay1@gmail.com>
2025-11-21 09:40:32 -0500

    chore: initialize sequelize structure and dynamic config

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME || "fintech_demo",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME_TEST || "fintech_demo_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
